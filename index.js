6a9c37184f3d893ae6e04eedbc06de9ad5bf2d5e
xauroxs <xauroxs@gmail.com>
2023-10-20 23:54:59 +0300

    create express app

---
+++ index.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const session = require("express-session");
+
+const app = express();
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
