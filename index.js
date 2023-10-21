9bfa781519cdafc12f759efc425a8e5323ed32f1
xauroxs <xauroxs@gmail.com>
2023-10-21 22:32:49 +0300

    apply router, use passportjs

--- index.js
+++ index.js
@@ -2,8 +2,20 @@ require("dotenv").config();
 
 const express = require("express");
 const session = require("express-session");
+const passport = require("passport");
+
+const router = require("./router");
+
+require("./auth");
 
 const app = express();
+
+app.use(session({ secret: process.env.SESSION_SECRET }));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use("/", router());
 
 const PORT = 3000;
 
